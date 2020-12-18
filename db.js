3a4e549330b8f10e43734dc4b6f7d63db924578f
crackerslekap <jeanesbailey1@gmail.com>
2020-12-18 19:34:41 +1000

    initial leaderboard implementation

--- db.js
+++ db.js
@@ -39,16 +39,26 @@ function submitTest(rawcpm, adjustedcpm, adjustedwpm, accuracy, dur, autocorrect
    updateResults();
 }
 
+function checkForDuplicates(array) {
+    let valuesAlreadySeen = []
+  
+    for (let i = 0; i < array.length; i++) {
+      let value = array[i]
+      if (valuesAlreadySeen.indexOf(value) !== -1) {
+        return true
+      }
+      valuesAlreadySeen.push(value)
+    }
+    return false
+  }
+  
+
 $(".acct-15.acct-30.acct-60.acct-120").on("click", updateDisplay);
 
 function updateLb(durSetting) {
-    const fifteens = document.querySelector(".lb-15");
-    const thirtys = document.querySelector(".lb-30");
-    const sixtys = document.querySelector(".lb-60");
-    const onetwentys = document.querySelector(".lb-120");
 
     var itemArr = [];
-
+    var toPush = []
     db.collection("users").get().then((response) => {
         response.forEach((doc) => {
             var tempId = doc.id;
@@ -57,12 +67,21 @@ function updateLb(durSetting) {
                 snapshot.forEach((doc) => {
                     /*db.collection("users").doc(tempId).collection("scores").*/
                     //if(itemArr.indexOf(tempUsername) === -1 ? itemArr.push(tempUsername) : console.log("already exists"));;
-                    var toPush = doc.data();
-                    itemArr.push(toPush);
+                    toPush.push(doc.data());
                 });
+                itemArr = itemArr.concat(toPush);
+                lbCont(itemArr, durSetting);
             });
         });
     });
+}
+
+function lbCont(itemArr, durSetting) {
+
+    const fifteens = document.querySelector(".lb-15");
+    const thirtys = document.querySelector(".lb-30");
+    const sixtys = document.querySelector(".lb-60");
+    const onetwentys = document.querySelector(".lb-120");
 
     if(durSetting == 15) {
         fifteens.setAttribute("class", "btn-large waves-effect waves-light grey lighten-1 black-text lb-15");
@@ -70,21 +89,181 @@ function updateLb(durSetting) {
         sixtys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-60");
         onetwentys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-120");
 
+        const container = document.querySelector(".lb-parent");
+        container.innerHTML = "";
+        itemArr.forEach((item) => {
+            if(item.durSetting == "15") {
+                if(document.querySelector("." + item.username)) {
+                    console.log("exists already");
+                } else {
+                    if(item.rawCPMs.length > 0) {
+                        var innerParent = document.createElement("tr");
+                        var userElem = document.createElement("td");
+                        var rawElem = document.createElement("td");
+                        var adjustElem = document.createElement("td");
+
+                        var adjustWpm = Math.max(...item.adjustWPMs);
+                        var tempAdjIndex = item.adjustWPMs.indexOf(adjustWpm);
+                        var rawWpm = item.rawCPMs[tempAdjIndex] / 5;
+
+                        userElem.innerHTML = item.username;
+                        rawElem.innerHTML = rawWpm;
+                        adjustElem.innerHTML = adjustWpm;
+                        innerParent.appendChild(userElem);
+                        innerParent.appendChild(rawElem);
+                        innerParent.appendChild(adjustElem);
+                        innerParent.setAttribute("class", item.username);
+                        container.appendChild(innerParent);
+                    } else {
+                        container.innerHTML = "";
+                    }
+                }
+
+            }
+        });
+
+        /*
+
+        for(var r = wpmArr.length - 1; r >= 0; r--) {
+            var innerParent = document.createElement("tr");
+            var recentRawElem = document.createElement("td");
+            var recentWpmElem = document.createElement("td");
+            var recentAccElem = document.createElement("td");
+            recentRawElem.innerHTML = Math.round(rawArr[r] / 5);
+            recentWpmElem.innerHTML = wpmArr[r];
+            recentAccElem.innerHTML = accArr[r];
+            innerParent.appendChild(recentRawElem);
+            innerParent.appendChild(recentWpmElem);
+            innerParent.appendChild(recentAccElem);
+            container.appendChild(innerParent);
+        }
+        */
+
+
     } else if(durSetting == 30) {
         thirtys.setAttribute("class", "btn-large waves-effect waves-light grey lighten-1 black-text lb-30");
         sixtys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-60");
         fifteens.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-15");
         onetwentys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-120");
+
+        
+        const container = document.querySelector(".lb-parent");
+        container.innerHTML = "";
+
+        itemArr.forEach((item) => {
+            if(item.durSetting == "30") {
+
+                if(document.querySelector("." + item.username)) {
+                    console.log("exists already");
+                } else {
+                    if(item.rawCPMs.length > 0) {
+                        var innerParent = document.createElement("tr");
+                        var userElem = document.createElement("td");
+                        var rawElem = document.createElement("td");
+                        
+                        var adjustWpm = Math.max(...item.adjustWPMs);
+                        var tempAdjIndex = item.adjustWPMs.indexOf(adjustWpm);
+                        var rawWpm = item.rawCPMs[tempAdjIndex] / 5;
+                        
+                        userElem.innerHTML = item.username;
+                        rawElem.innerHTML = rawWpm;
+                        adjustElem.innerHTML = adjustWpm;
+                        innerParent.appendChild(userElem);
+                        innerParent.appendChild(rawElem);
+                        innerParent.appendChild(adjustElem);
+                        innerParent.setAttribute("class", item.username);
+                        container.appendChild(innerParent);
+                    } else {
+                        container.innerHTML = "";
+                    }
+                }
+
+            }
+        });
+
     } else if(durSetting == 60) {
         sixtys.setAttribute("class", "btn-large waves-effect waves-light grey lighten-1 black-text lb-60");
         thirtys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-30");
         fifteens.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-15");
         onetwentys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-120");
+
+        
+        const container = document.querySelector(".lb-parent");
+        container.innerHTML = "";
+
+        itemArr.forEach((item) => {
+            if(item.durSetting == "60") {
+
+                if(document.querySelector("." + item.username)) {
+                    console.log("exists already");
+                } else {
+                    if(item.rawCPMs.length > 0) {
+                        var innerParent = document.createElement("tr");
+                        var userElem = document.createElement("td");
+                        var rawElem = document.createElement("td");
+                        var adjustElem = document.createElement("td");
+                        
+                        var adjustWpm = Math.max(...item.adjustWPMs);
+                        var tempAdjIndex = item.adjustWPMs.indexOf(adjustWpm);
+                        var rawWpm = item.rawCPMs[tempAdjIndex] / 5;
+                        
+                        userElem.innerHTML = item.username;
+                        rawElem.innerHTML = rawWpm;
+                        adjustElem.innerHTML = adjustWpm;
+                        innerParent.appendChild(userElem);
+                        innerParent.appendChild(rawElem);
+                        innerParent.appendChild(adjustElem);
+                        innerParent.setAttribute("class", item.username);
+                        container.appendChild(innerParent);
+                    } else {
+                        container.innerHTML = "";
+                    }
+                }
+
+            }
+        });
+
     } else if(durSetting == 120) {
         onetwentys.setAttribute("class", "btn-large waves-effect waves-light grey lighten-1 black-text lb-120");
         sixtys.setAttribute("class", "btn-large waves-effect waves-light lighten-1 lb-60");
         thirtys.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-30");
         fifteens.setAttribute("class", "btn-large waves-effect waves-light teal lighten-1 lb-15");
+
+        
+        const container = document.querySelector(".lb-parent");
+        container.innerHTML = "";
+
+        itemArr.forEach((item) => {
+            if(item.durSetting == "120") {
+                if(document.querySelector("." + item.username)) {
+                    console.log("exists already");
+                } else {
+                    if(item.rawCPMs.length > 0) {
+                        var innerParent = document.createElement("tr");
+                        var userElem = document.createElement("td");
+                        var rawElem = document.createElement("td");
+                        var adjustElem = document.createElement("td");
+                        
+                        var adjustWpm = Math.max(...item.adjustWPMs);
+                        var tempAdjIndex = item.adjustWPMs.indexOf(adjustWpm);
+                        var rawWpm = item.rawCPMs[tempAdjIndex] / 5;
+                        
+                        userElem.innerHTML = item.username;
+                        rawElem.innerHTML = rawWpm;
+                        adjustElem.innerHTML = adjustWpm;
+                        innerParent.appendChild(userElem);
+                        innerParent.appendChild(rawElem);
+                        innerParent.appendChild(adjustElem);
+                        innerParent.setAttribute("class", item.username);
+                        container.appendChild(innerParent);
+                    } else {
+                        container.innerHTML = "";
+                    }
+                }
+
+            }
+        });
+
     }
 }
 
@@ -99,7 +278,6 @@ db.collection("users").get().then((doc) => {
         */
 
 function updateDisplay(durSetting) {
-    console.log("function dumbo");
     let user = auth.currentUser;
     const highestElem = document.querySelector(".highest-wpm");
     const avgWpmElem = document.querySelector(".avg-wpm");
